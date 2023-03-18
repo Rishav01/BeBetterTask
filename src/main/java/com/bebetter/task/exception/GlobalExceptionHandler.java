2329730b66a2c910df797223d985dc01ccb4605c
Rishav <mrishav72@gmail.com>
2023-03-18 10:11:44 +0530

    Improved ExceptionHandling for RequestDTO

--- src/main/java/com/bebetter/task/exception/GlobalExceptionHandler.java
+++ src/main/java/com/bebetter/task/exception/GlobalExceptionHandler.java
@@ -1,10 +1,13 @@
 package com.bebetter.task.exception;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
-import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import javax.validation.ConstraintViolation;
 import javax.validation.ConstraintViolationException;
@@ -13,32 +16,36 @@ import java.util.List;
 import java.util.Set;
 import java.util.stream.Collectors;
 
-@ControllerAdvice
+@RestControllerAdvice
+@Slf4j
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(BeBetterBadRequestException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
     public ResponseEntity<BeBetterErrorResponse> handleBadRequestException(BeBetterBadRequestException exception){
         return new ResponseEntity(exception.getBeBetterErrorResponse(), HttpStatus.BAD_REQUEST);
     }
 
     @ExceptionHandler(BeBetterNotFoundException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
     public ResponseEntity<BeBetterErrorResponse> handleNotFoundException(BeBetterNotFoundException exception){
         return new ResponseEntity(exception.getBeBetterErrorResponse(), HttpStatus.NOT_FOUND);
     }
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
     public ResponseEntity<BeBetterErrorResponse> handleMethodArgumentNotValidException(MethodArgumentNotValidException
                                                                                        methodArgumentNotValidException){
-        List<String> errors = methodArgumentNotValidException.getBindingResult().getFieldErrors()
-                .stream().map(x -> x.getDefaultMessage()).collect(Collectors.toList());
+        List<FieldError> errors = methodArgumentNotValidException.getBindingResult().getFieldErrors();
         return new ResponseEntity<>(BeBetterErrorResponse.builder()
                 .errorCode(ErrorCode.VALIDATION_FAILED_RB)
-                .exceptionMessage(errors.size()>1 ? getAllErrorFormatted(errors) : errors.get(0))
+                .exceptionMessage(getAllErrorFormatted(errors))
                 .timeStamp(LocalDateTime.now())
                 .build(), HttpStatus.BAD_REQUEST);
     }
 
     @ExceptionHandler(ConstraintViolationException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
     public ResponseEntity<BeBetterErrorResponse> handleConstraintViolationException(ConstraintViolationException exception){
         Set<ConstraintViolation<?>> voilations = exception.getConstraintViolations();
         String message = getConcatenatedErrorMessage(voilations);
@@ -50,7 +57,10 @@ public class GlobalExceptionHandler {
     }
 
     @ExceptionHandler(RuntimeException.class)
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
     public ResponseEntity<BeBetterErrorResponse> handleInternalServerException(RuntimeException exception){
+        log.error("INTERNAL_SERVER_ERROR occurred");
+        exception.printStackTrace();
         return new ResponseEntity(BeBetterErrorResponse.builder()
                 .errorCode(ErrorCode.INTERNAL_SERVER_ERROR)
                 .exceptionMessage(ErrorCode.INTERNAL_SERVER_ERROR.getDescription())
@@ -58,8 +68,9 @@ public class GlobalExceptionHandler {
                 .build(), HttpStatus.INTERNAL_SERVER_ERROR);
     }
 
-    private String getAllErrorFormatted(List<String> error){
-        return "[".concat(error.stream().reduce((x1, x2) -> x1.concat(" and ").concat(x2)).get()).concat("]");
+    private String getAllErrorFormatted(List<FieldError> error){
+        return error.stream().map(x -> x.getField().concat(": ").concat(x.getDefaultMessage())).collect(Collectors.toList())
+                .stream().reduce((y1, y2) -> y1.concat(" and ").concat(y2)).get();
     }
 
     private String getConcatenatedErrorMessage(Set<ConstraintViolation<?>> voilations){
